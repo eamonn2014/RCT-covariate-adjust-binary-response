969244b1e1d09e1c7bd2dc73ff00b030abf605e9
eamonn1 <eamonnojbrien1@gmail.com>
2020-10-03 08:31:09 +0100

    will load rdata by click need to supply url added ref

--- load-direct-from-rdata/app.R
+++ load-direct-from-rdata/app.R
@@ -1,13 +1,13 @@
 
 #shiny app that loads rdata by one click just need to supply url
-
+#https://stackoverflow.com/questions/21813773/r-shiny-uploading-a-file-on-button-click
 rm(list=ls())
 pp<-"https://github.com/eamonn2014/RCT-covariate-adjust-binary-response/raw/master/cov-adj-binary-response/A%205000%20default%20settings%20theta%20log1.5%20-1.00%20-0.67%20-0.43.Rdata"
 #load(url(pp))
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI  
 ui <- fluidPage(
     titlePanel("Uploading a file from rdata directly"),
     sidebarLayout(
@@ -24,7 +24,7 @@ ui <- fluidPage(
         ))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Define server logic required to draw a histogram
+# Define server logic  
 options(shiny.maxRequestSize = 9*1024^2)
 
 server <- function(input, output) {
