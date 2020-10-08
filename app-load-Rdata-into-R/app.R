84e9c6f25f2bed499056047b27bff353a93cc776
eamonn1 <eamonnojbrien1@gmail.com>
2020-10-08 14:38:29 +0100

    adding new tab basic log reg explanation

--- app-load-Rdata-into-R/app.R
+++ app-load-Rdata-into-R/app.R
@@ -7,29 +7,67 @@
 #    http://shiny.rstudio.com/
 #
 
+rm(list=ls())
+pp<-"https://github.com/eamonn2014/RCT-covariate-adjust-binary-response/raw/master/cov-adj-binary-response/A%205000%20default%20settings%20theta%20log1.5%20-1.00%20-0.67%20-0.43.Rdata"
+#load(url(pp))
+
+
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
     titlePanel("Uploading Files"),
     sidebarLayout(
+        # sidebarPanel(
+        #     fileInput('file1', 'Choose file to upload',
+        #               accept = c(
+        #                   '.Rdata'
+        #               )
+        #     ),
+        #     tags$hr(),
+        #     
+        #     tags$hr(),
+        #     
+        # ),
+        # mainPanel(
+        #     tableOutput('contents'),
+        #     tableOutput('contents2'),
+        #     tableOutput('contents3'),
+        #     #div( verbatimTextOutput("x") )
+        # )
+        # 
+        
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+        
+        
         sidebarPanel(
-            fileInput('file1', 'Choose file to upload',
-                      accept = c(
-                          '.Rdata'
-                      )
-            ),
-            tags$hr(),
-            
-            tags$hr(),
-            
+           # textInput("path", "File:"),
+           # actionButton("browse", "Browse"),
+            tags$br(),
+            actionButton("upload", "Upload Data")
         ),
+        
         mainPanel(
-            tableOutput('contents'),
-            tableOutput('contents2'),
-            tableOutput('contents3'),
-            #div( verbatimTextOutput("x") )
+            verbatimTextOutput('content')
         )
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+        
+        
+        
+        
         
         
         
@@ -44,37 +82,68 @@ options(shiny.maxRequestSize = 9*1024^2)
 server <- function(input, output) {
     
     
-    output$contents <- renderTable({
-        # input$file1 will be NULL initially. After the user selects
-        # and uploads a file, it will be a data frame with 'name',
-        # 'size', 'type', and 'datapath' columns. The 'datapath'
-        # column will contain the local filenames where the data can
-        # be found.
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    contentInput <- reactive({ 
         
-        inFile <- input$file1
+       if(input$upload == 0) return()
         
-        
+       isolate({
+           isfar <-  load(url(pp))
+                get((isfar)[12])
+      })
     })
     
-    output$contents2 <- renderTable({
-        
-        inFile <- input$file1
-        if (is.null(inFile))
-            return(NULL)
-        load(inFile$datapath)
-        
+    output$content <- renderPrint({
+        contentInput()
     })
     
     
-    output$contents3 <- renderTable({
-        
-        inFile <- input$file1
-        if (is.null(inFile))
-            return(NULL)
-        isfar <- (load(inFile$datapath))
-        get((isfar)[12])
-    })
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
+    
+    
+    
+    
+    
+    
+    
+    
+    # 
+    # 
+    # 
+    # 
+    # output$contents <- renderTable({
+    #     # input$file1 will be NULL initially. After the user selects
+    #     # and uploads a file, it will be a data frame with 'name',
+    #     # 'size', 'type', and 'datapath' columns. The 'datapath'
+    #     # column will contain the local filenames where the data can
+    #     # be found.
+    #     
+    #     inFile <- input$file1
+    #     
+    #     
+    # })
+    # 
+    # output$contents2 <- renderTable({
+    #     
+    #     inFile <- input$file1
+    #     if (is.null(inFile))
+    #         return(NULL)
+    #     load(inFile$datapath)
+    #     
+    # })
+    # 
+    # 
+    # output$contents3 <- renderTable({
+    #     
+    #     inFile <- input$file1
+    #     if (is.null(inFile))
+    #         return(NULL)
+    #     isfar <- (load(inFile$datapath))
+    #     get((isfar)[12])
+    # })
+    # 
     
     
     
