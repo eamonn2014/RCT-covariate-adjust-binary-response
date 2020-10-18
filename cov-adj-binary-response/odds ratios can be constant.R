93766a56af7b72f462b027f07d2e281c2e89f58b
eamonn1 <eamonnojbrien1@gmail.com>
2020-10-18 14:33:53 +0100

    adding a plot

--- cov-adj-binary-response/odds ratios can be constant.R
+++ cov-adj-binary-response/odds ratios can be constant.R
@@ -1,15 +1,19 @@
 
-OR<-2
-prior.p<-seq(0.1,0.9, by=0.1)
-prior.odds<-prior.p/(1-prior.p)
-post.odds<-prior.odds*OR
-post.p<-(post.odds/(1+post.odds))
+OR<-5
+prior.p <- c(0.01,seq(0.05,0.95, by=0.05), .99)
+prior.odds <- prior.p/(1-prior.p)
+post.odds <- prior.odds*OR
+post.p <- (post.odds/(1+post.odds))
 
 f<-cbind(prior.p, prior.odds, OR, post.odds, post.p)
 colnames(f)<-c("prior prob.","prior odds","odds ratio","post odds","predicted prob.")
 print(f, digits=2)
 
+par(pty="s")
+plot(f[,1], f[,5], xlab="prior prob.", ylab='post.prob.', type = "l" , ylim=c(0,1) , xlim=c(0,1))
+abline(0,1, lty=2)#
 
+#lines( f[,2], f[,4]       )
 #if the OR is 1.5 for a 0.1 unit change then...
 #for a x unit change 'want'
 
